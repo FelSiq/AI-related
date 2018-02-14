68fd8816d12cea6a1c6ad3f3d338ac62ec25fd47
FelSiq <felipe.siqueira@usp.br>
2018-02-14 02:32:45 -0200

    tic-tac-toe hotfix + interface update.

--- minimax/tictactoe.py
+++ minimax/tictactoe.py
@@ -31,43 +31,43 @@ class tictactoe:
 					return True
 		return False
 
-	def _minValue(self, alpha, beta):
-		gameValue = self._getStaticValue()
+	def _minValue(self, alpha, beta, depth):
+		gameValue = self._getStaticValue(depth)
 		if gameValue or not self._checkMoves():
 			return gameValue
 		
 		for x, y in self._getNextMoves():
 			self.gameMat[y][x][0] = -1.0
-			beta = min(beta, self._maxValue(alpha, beta))
+			beta = min(beta, self._maxValue(alpha, beta, depth+1))
 			self.gameMat[y][x][0] = +0.0
 			if alpha >= beta:
 				return beta
 
 		return beta
 
-	def _maxValue(self, alpha, beta):
-		gameValue = self._getStaticValue()
+	def _maxValue(self, alpha, beta, depth):
+		gameValue = self._getStaticValue(depth)
 		if gameValue or not self._checkMoves():
 			return gameValue
 
 		for x, y in self._getNextMoves():
 			self.gameMat[y][x][0] = +1.0
-			alpha = max(alpha, self._minValue(alpha, beta))
+			alpha = max(alpha, self._minValue(alpha, beta, depth+1))
 			self.gameMat[y][x][0] = +0.0
 			if alpha >= beta:
 				return alpha
 
 		return alpha
 
-	def _firstCallMaxValue(self, alpha, beta):
-		gameValue = self._getStaticValue()
+	def _firstCallMaxValue(self, alpha, beta, depth):
+		gameValue = self._getStaticValue(depth)
 		if gameValue or not self._checkMoves():
 			return gameValue
 
 		bestX, bestY = None, None
 		for x, y in self._getNextMoves():
 			self.gameMat[y][x][0] = +1.0
-			returnedValue = self._minValue(alpha, beta)
+			returnedValue = self._minValue(alpha, beta, depth+1)
 			self.gameMat[y][x][0] = +0.0
 			if (bestX == None and alpha <= returnedValue) or alpha < returnedValue:
 				alpha = returnedValue
@@ -79,7 +79,7 @@ class tictactoe:
 
 		return bestX, bestY
 
-	def _getStaticValue(self):
+	def _getStaticValue(self, depth):
 		for p in self.gameRefs:
 			aux = 0
 			for n in p:
@@ -87,18 +87,22 @@ class tictactoe:
 			if abs(aux) == 3.0:
 				if aux > 0:
 					# Machine won
-					return 100.0
+					return 9.0 - depth
 				# Player won
-				return -100.0
+				return -9.0 + depth
 		# Nobody won
 		return 0.0
 
 	def move(self):
-		return self._firstCallMaxValue(-math.inf, math.inf)
+		return self._firstCallMaxValue(-math.inf, math.inf, 0)
 
 	def _printGame(self):
 		pos = 0
+		print(colorama.Fore.GREEN + '\u250F', '\u2501' * 11, 
+			'\u2513' + colorama.Fore.RESET, sep='')
 		for m in self.gameMat:
+			print(end=colorama.Fore.GREEN + '\u2503 ' + 
+				colorama.Fore.RESET)
 			for n in m:
 				symbol = colorama.Fore.GREEN + str(pos + 1)
 				if n[0] > 0.0:
@@ -106,15 +110,26 @@ class tictactoe:
 				elif n[0] < 0.0:
 					symbol = colorama.Fore.BLUE + 'O'
 
-				print(symbol + colorama.Fore.RESET, end='\t')
+				print(symbol + colorama.Fore.RESET, end=' ')
 				if (pos + 1) % 3 == 0:
-					print()
+					print(colorama.Fore.GREEN + '\u2503' + 
+						colorama.Fore.RESET)
+				else:
+					print(end='  ')
 				pos += 1
-		print()
+		print(colorama.Fore.GREEN + '\u2517', '\u2501' * 11, 
+			'\u251B' + colorama.Fore.RESET, sep='')
 
 	def play(self):
 		playerTurn = random.random() >= 0.5
-		gameState = self._getStaticValue()
+
+		if playerTurn:
+			startMessage = 'Human goes first.'
+		else:
+			startMessage = 'Machine starts.'
+		print('The tic-tac-toe gods plays an die.', startMessage)
+
+		gameState = self._getStaticValue(0)
 		while self._checkMoves() and not gameState:
 			if playerTurn:
 				self._printGame()
@@ -138,7 +153,7 @@ class tictactoe:
 				self.gameMat[bestY][bestX][0] = 1.0
 				playerTurn = True
 
-			gameState = self._getStaticValue()
+			gameState = self._getStaticValue(0)
 		self._printGame()
 
 		if gameState > 0.0:
